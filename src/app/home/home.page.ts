fa43b3ceace401a3605b91ab3e937f435ac2f66a
Leon Huzen <leonhuzen@gmail.com>
2019-04-13 14:53:16 +0200

    2screens

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { IonSlides, IonSelect } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // this.slides.slideNext();
+    // this.slides.slideNext();
+  }
 
+  onNextButtonClick = () =>
+    this.slides.slideNext();
+
+  onCarSelectButtonClick = () =>
+    this.carSelector.open();
+
+  @ViewChild('slides') slides : IonSlides;
+  @ViewChild('carSelector') carSelector : IonSelect;
+
+  slideOpts = {
+    effect: 'flip'
+  };
 }
