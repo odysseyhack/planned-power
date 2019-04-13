bcfef8a7ff51801f18a720090e61f59695c938e4
gerhardboer <gboer@ilionx.com>
2019-04-13 10:51:33 +0200

    PowerPromise test

--- test/PowerPromise.js
+++ test/PowerPromise.js
@@ -7,14 +7,17 @@ const PowerPromise = artifacts.require("PowerPromise");
 contract("PowerPromise Test", async accounts => {
   let powerPromise;
 
-  it("should store and create an order", async () => {
+  it("should store the promise and be retrievable", async () => {
     // MUst change default from account, because AdminUpgrade
     await PowerPromise.defaults({from: accounts[0]});
     powerPromise = await PowerPromise.deployed();
 
-    await powerPromise.promise('9725', '2019-01-01', '15:00', '2019-01-01', '18:00', 60, 30, 100);
-    const promises = await powerPromise.retrieve();
+    let promises = await powerPromise.retrieve();
+    expect(promises.length).to.equal(0);
 
+
+    await powerPromise.promise('9725', '2019-01-01', '15:00', '2019-01-01', '18:00', 60, 30, 100);
+    promises = await powerPromise.retrieve();
     expect(promises.length).to.equal(1);
   });
 
