736bf1852238e3b0207c5722fd63bd114f5a7407
Michel Vollebregt <mi.vollebregt@gmail.com>
2019-04-13 22:49:29 +0200

    Changed app display name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  blockchainType: undefined,
+  blockchainType: 'vmware',
   useProxy: false,
+  path: 'https://mgmt.blockchain.vmware.com/blockchains/077d11b9-7147-4022-9af8-b937574d3cd5/api/concord/eth',
   network: '5000',
-  path: 'https://mgmt.blockchain.vmware.com/blockchains/077d11b9-7147-4022-9af8-b937574d3cd5/api/concord/eth'
 };
