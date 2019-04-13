7aaf7a029a4f55761a09ac5a0adea7fceed631c1
gerhardboer <gboer@ilionx.com>
2019-04-13 20:33:16 +0200

    some form inputs for varied data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { catchError, flatMap } from 'rxjs/operators';
 import { PowerPromiseService } from '../core/blockchain/power-promise.service';
 import { throwError } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,17 @@ import { throwError } from 'rxjs';
   styleUrls: [ 'home.page.scss' ],
 })
 export class HomePage implements OnInit {
+  sending: boolean;
+  promiseForm = new FormGroup({
+    region: new FormControl('Beijum'),
+    startday:  new FormControl('01-01-1010'),
+    starttime:  new FormControl('11:30'),
+    endday:  new FormControl('02-02-2020'),
+    endtime:  new FormControl('23:34'),
+    fullcapicity:  new FormControl(100),
+    minimalcapacity: new FormControl(20),
+    begincapacity:  new FormControl(80)
+  });
 
   constructor(private powerPromiseService: PowerPromiseService) {
   }
@@ -18,24 +30,32 @@ export class HomePage implements OnInit {
   }
 
   async sendAndRetrieve() {
+    this.sending = true;
 
-    this.powerPromiseService.promise({
-      region: 'Beijum',
-      startday: '01-01-1010',
-      starttime: '11:30, ',
-      endday: '02-02-2020',
-      endtime: '23:34',
-      fullcapicity: 100,
-      minimalcapacity: 20,
-      begincapacity: 80
-    }).pipe(
+    let promise = this.promiseForm.value;
+    // let promise = {
+    //   region: 'Beijum',
+    //   startday: '01-01-1010',
+    //   starttime: '11:30, ',
+    //   endday: '02-02-2020',
+    //   endtime: '23:34',
+    //   fullcapicity: 100,
+    //   minimalcapacity: 20,
+    //   begincapacity: 80
+    // }
+    //
+    this.powerPromiseService.promise(promise).pipe(
       flatMap(() => this.powerPromiseService.length()),
       flatMap(length => this.powerPromiseService.retrieve(length)),
       catchError(err => {
         console.error(err);
+        this.sending = false;
         return throwError(err);
       }),
-    ).subscribe(console.table);
+    ).subscribe((data) => {
+      console.table(data);
+      this.sending = false;
+    });
   }
 
   async retrieve() {
