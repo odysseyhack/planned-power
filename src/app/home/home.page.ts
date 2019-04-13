9da5dffce70d6dbb4983323876d285e68567684b
Leon Huzen <leonhuzen@gmail.com>
2019-04-13 22:17:07 +0200

    screens

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,59 @@
 import { Component, AfterViewInit, ViewChild } from '@angular/core';
-import { IonSlides, IonSelect } from '@ionic/angular';
+import { IonSlides, IonSelect, IonRange } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: [ 'home.page.scss' ],
 })
-export class HomePage  implements AfterViewInit {
-  ngAfterViewInit(): void {
-    // this.slides.slideNext();
-    // this.slides.slideNext();
+export class HomePage implements AfterViewInit {
+	ngAfterViewInit (): void {
+    this.slides.length().then(x => this.sliderLength = x);
+
+    this.slides.ionSlideWillChange.subscribe(() => {
+      this.slides.getActiveIndex().then(beforeChangeIndex => {
+        this.isSliderAtEnd = ++beforeChangeIndex === this.sliderLength;
+      });
+    })
   }
 
-  onNextButtonClick = () =>
-    this.slides.slideNext();
+	onNextButtonClick = () => this.slides.slideNext();
 
-  onCarSelectButtonClick = () =>
-    this.carSelector.open();
+  onCarSelectButtonClick = () => this.carSelector.open();
 
-  @ViewChild('slides') slides : IonSlides;
-  @ViewChild('carSelector') carSelector : IonSelect;
+  completeWizard = () => this.wizardCompleted = true;
 
-  slideOpts = {
-    effect: 'flip'
-  };
+	onMondayClick = () => this.mondayStatus = !this.mondayStatus;
+	onTuesdayClick = () => this.tuesdayStatus = !this.tuesdayStatus;
+	onWednesdayClick = () => this.wednesdayStatus = !this.wednesdayStatus;
+	onThursdayClick = () => this.thursdayStatus = !this.thursdayStatus;
+	onFridayClick = () => this.fridayStatus = !this.fridayStatus;
+	onSaturdayClick = () => this.saturdayStatus = !this.saturdayStatus;
+  onSundayClick = () => this.sundayStatus = !this.sundayStatus;
+
+	@ViewChild('slides') slides: IonSlides;
+  @ViewChild('carSelector') carSelector: IonSelect;
+  @ViewChild('paintRange') paintRange: IonRange;
+  @ViewChild('hourRange') hourRange: IonRange;
+
+  mondayStatus = true;
+  tuesdayStatus = true;
+  wednesdayStatus = true;
+  thursdayStatus = true;
+  fridayStatus = true;
+  saturdayStatus = false;
+  sundayStatus = false;
+
+  isSliderAtEnd = false;
+  sliderLength = 0;
+
+  wizardCompleted = true;
+  showDashboard = true;
+
+  findChargers = () => this.showDashboard = !this.showDashboard;
+
+	slideOpts = {
+    effect: 'flip',
+    touchRatio: 0
+	};
 }
